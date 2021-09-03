ab74c3d602f4f929668cea1e6bd44aa0ea522af0
Lelectrolux <Lelectrolux@users.noreply.github.com>
2021-09-04 01:46:46 +0200

    Console renderer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Game from './Game.js'
-window.game = new Game()
-
 import SquareRenderer from './SquareRenderer.js'
+import ConsoleRenderer from './ConsoleRenderer.js'
+
+window.game = new Game()
 new SquareRenderer(game, document.getElementById('snake-square'))
+new ConsoleRenderer(game)
