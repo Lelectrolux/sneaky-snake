038487a2d2bd1ee9321cd35419684049a0e05314
Lelectrolux <Lelectrolux@users.noreply.github.com>
2021-09-15 16:22:35 +0200

    Use alpine.js in index.html

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import Game from './Game'
 import SquareRenderer from './SquareRenderer'
 
 let game = new Game()
-new SquareRenderer(game, <HTMLCanvasElement>document.getElementById('square'))
+new SquareRenderer(game, <HTMLCanvasElement>document.getElementById('squares'))
 new ConsoleRenderer(game)
 new AsciiRenderer(game, <HTMLPreElement>document.getElementById('ascii'))
 
