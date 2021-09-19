08b6029ed305363408d10221e41c8ab8f85d2a34
Lelectrolux <Lelectrolux@users.noreply.github.com>
2021-09-19 20:32:19 +0200

    Remove SquareRenderer.ts

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,12 @@ import AsciiRenderer from './AsciiRenderer'
 import ConsoleRenderer from './ConsoleRenderer'
 import Game from './Game'
 import SpriteRenderer from "./SpriteRenderer"
-import SquareRenderer from './SquareRenderer'
 
 declare global {
   interface Window {
     Game,
     ConsoleRenderer,
     AsciiRenderer,
-    SquareRenderer,
     SpriteRenderer,
   }
 }
@@ -17,5 +15,4 @@ declare global {
 window.Game = Game
 window.ConsoleRenderer = ConsoleRenderer
 window.AsciiRenderer = AsciiRenderer
-window.SquareRenderer = SquareRenderer
 window.SpriteRenderer = SpriteRenderer
