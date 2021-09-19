b21ae5a1d50f3b1071ee5151c34d10b81c0b3e90
Lelectrolux <Lelectrolux@users.noreply.github.com>
2021-09-19 20:33:17 +0200

    Add color support and lose state to AsciiRenderer.ts

--- src/AsciiRenderer.ts
+++ src/AsciiRenderer.ts
@@ -1,9 +1,18 @@
-import BaseRenderer from "./BaseRenderer";
 import Game, { Direction, GameState } from "./Game";
 
 const repeat = require('lodash/repeat.js')
 const chunk = require('lodash/chunk.js')
 
+type Classes = {
+  apple: string,
+  head: string,
+  snake: string,
+  board: string,
+}
+
+function span(text: string | number, classes: string) {
+  return `<span class="${classes}">${text}</span>`
+}
 function directionToArrow(direction: Direction): string {
   return {
     [Direction.Up]: '↑',
@@ -13,41 +22,63 @@ function directionToArrow(direction: Direction): string {
   }[direction]
 }
 
-export default class AsciiRenderer extends BaseRenderer {
+export default class AsciiRenderer {
   cols: number
   rows: number
   pre: HTMLPreElement
+  classes: Classes
 
-  constructor(game: Game, pre: HTMLPreElement) {
-    super()
-
+  constructor(
+      game: Game,
+      pre: HTMLPreElement,
+      classes: Classes = {
+        apple: 'text-red-600',
+        head: 'text-green-700',
+        snake: 'text-green-500',
+        board: 'text-gray-400'
+      }
+  ) {
     this.cols = game.cols
     this.rows = game.rows
     this.pre = pre
+    this.classes = classes
 
-    this.init(game)
+    this.render(game.state)
+    game.events.on('afterTick', state => this.render(state))
+    game.events.on('directionChanged', state => this.render(state))
+    game.events.on('lost', state => this.render(state))
+    game.events.on('pause', state => this.render(state))
+    game.events.on('play', state => this.render(state))
   }
 
-  public render({ snake, apple, score }: GameState) {
-    let data = new Array(this.rows * this.cols).fill(' ')
+  public render({ snake, apple, score, ticks, direction, lost, running }: GameState) {
+    let data = new Array(this.rows * this.cols).fill('·')
     data = chunk(data, this.cols)
 
-    snake.forEach(({ x, y, direction }, i) => {
-      data[y][x] = i === 0 || direction === snake[i - 1].direction
-          ? directionToArrow(direction)
-          : directionToArrow(snake[i - 1].direction)
+    snake.forEach(({ x, y, direction: dir }, i) => {
+      if (i === 0) { // head
+        data[y][x] = span(
+            directionToArrow(direction),
+            lost || running ? this.classes.head : '')
+      } else { // body
+        data[y][x] = span(
+            directionToArrow(dir !== snake[i - 1].direction ? snake[i - 1].direction : dir),
+            lost || running ? this.classes.snake : '')
+      }
     })
 
-    data[apple.y][apple.x] = '@'
+    data[apple.y][apple.x] = span('@', lost || running ? this.classes.apple : '')
 
-    let topBorder = `+${repeat('—', this.cols)}+`
-    let scoreText = `[${score}@]`
-    let bottomBorder = `+${repeat('—', this.cols - scoreText.length)}${scoreText}+`
+    let divider = `+${repeat('―', this.cols)}+`
 
-    this.pre.innerText = [
-      topBorder,
-      ...data.map(line => `|${line.join('')}|`),
-      bottomBorder
+    this.pre.innerHTML = [
+      `[${span(directionToArrow(direction), this.classes.head)}][${span(score, this.classes.apple)}][${span(ticks, this.classes.board)}]`,
+
+      `<span class="${lost ? this.classes.apple : running ? '' : this.classes.board}">${divider}`,
+
+      ...data.map(line => `|${span(line.join(''), this.classes.board)}|`),
+
+      `${divider}</span>`
     ].join('\n')
   }
 }
